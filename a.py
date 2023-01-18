797739e71729ca3e82723415a6c68f862c7ff206
hyunin3 <stock4share2@gmail.com>
2023-01-18 23:25:31 +0900

    sad

---
+++ a.py
@@ -0,0 +1,8 @@
+#print(1,2,3)
+num=int(input())
+# a=range(1,num+1)
+# b=str(a)
+# print(b)
+a=list(range(1,num+1))
+for i in a:
+    print(i,end =', ')
---
+++ 거꾸로 세로로 출력하기.py
@@ -0,0 +1,7 @@
+num=int(input())
+i=num
+while i<=num:
+    i=i-1
+    if i==0:
+        break
+    print(i)    
